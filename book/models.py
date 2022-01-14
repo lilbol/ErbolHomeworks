d46d61d4f182017f0f601d57907f05337e498562
lilbol <erba0006@gmail.com>
2022-01-14 17:01:11 +0600

    Khalilov Erbol

--- book/models.py
+++ book/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 class Book(models.Model):
+    GENRE_CHOICE = (
+        ('Detective', 'Detective'),
+        ('Romantic', 'Romantic'),
+        ('Fantasy', 'Fantasy'),
+        ('Manga', 'Manga')
+    )
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='')
+    genre = models.CharField(choices=GENRE_CHOICE, max_length=100)
     created_day = models.DateTimeField(auto_now_add=True)
     updated_day = models.DateTimeField(auto_now=True)
--- book/views.py
+++ book/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . import models
 
 def book_all(requests):
     book = models.Book.objects.all()
     return render(requests, 'book_list.html', {'book': book})
+
+
+def book_detail(requests, id):
+    books = get_object_or_404(models.Book, id=id)
+    return render(requests, 'book_detail.html', {'books': books})
